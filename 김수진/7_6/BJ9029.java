ad7413725f908bcb3de0cc7d341b0c0b022f5309
hula-la <88833439+hula-la@users.noreply.github.com>
2022-08-28 23:53:57 +0900

    Add files via upload

---
+++ 김수진/7_6/BJ9029.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ9029 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken());
+            int L = Integer.parseInt(st.nextToken());
+            int H = Integer.parseInt(st.nextToken());
+        }
+    }
+}
