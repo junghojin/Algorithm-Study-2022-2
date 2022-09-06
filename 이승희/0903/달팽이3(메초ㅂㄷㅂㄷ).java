ffdcb92af79e9de6c57fe8e50a718bbe32685f83
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-09-07 05:39:48 +0900

    BOJ - 달팽이3(1959번) - 구현

--- 이승희/0903/달팽이3(메초ㅂㄷㅂㄷ).java
+++ 이승희/0903/달팽이3(메초ㅂㄷㅂㄷ).java
@@ -1,56 +1,41 @@
 // BOJ - 달팽이3(1959번)
-// 메모리 초과 ㅂㄷㅂㄷ 해결중 ㅎㅎ..
+// 알고리즘엔 수학이 최고다..
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main_1959 {
     public static int n, m;
-    public static boolean[][] map;
-    public static int[] dx = {0, 1, 0, -1};
-    public static int[] dy = {1, 0, -1, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] data = br.readLine().split(" ");
         int n = Integer.parseInt(data[0]);
         int m = Integer.parseInt(data[1]);
-        map = new boolean[n][m];
-        int nx = 0;
-        int ny = 0;
-        int dir = 0;
-        int cnt = 0;
-        map[nx][ny] = true;
 
-        long[] ans = new long[2];
-        int check_cnt = 0;
-        while (true){
-            if(check_cnt >= 4){
-                System.out.println(cnt-4);
-                System.out.println((nx+1)+" "+(ny+1));
-                break;
-            }
-            nx += dx[dir];
-            ny += dy[dir];
+        long min_value = (Math.min(n, m)-1) / 2;
+        long x = min_value+1;
+        long y = min_value+1;
 
-            if(nx < 0 || nx >= n || ny < 0 || ny >= m){
-                nx -= dx[dir];
-                ny -= dy[dir];
-                cnt++;
-                dir = (dir+1)%4;
-                continue;
-            } else if(map[nx][ny]){
-                check_cnt++;
-                cnt++;
-                nx -= dx[dir];
-                ny -= dy[dir];
-                dir = (dir+1)%4;
-                continue;
-            }
-            if(!map[nx][ny] && check_cnt >= 1){
-                check_cnt = 0;
-            }
-            map[nx][ny] = true;
+        long cnt = min_value*4;
+        n -= 2*min_value;
+        m -= 2*min_value;
 
+        if(n == 1){
+            y += (m-1);
+        } else if(m == 1){
+            cnt++;
+            x += (n-1);
+        } else if(n == 2){
+            cnt += 2;
+            x++;
+        } else {
+            cnt += 3;
+            x++;
         }
+
+
+        System.out.println(cnt);
+        System.out.println(x+" "+y);
     }
 }
